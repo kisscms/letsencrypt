433a84df9e68cce4d1bfcd1a72c57e6990225a08
Makis Tracend <makis.tracend@gmail.com>
2016-02-14 17:09:07 -0800

    + helper

---
+++ helpers/letsencrypt.php
@@ -0,0 +1,30 @@
+<?php
+
+/* Let's Encrypt SSL */
+
+class LetsEncrypt {
+
+	public function acmeToken( $token ){
+		$file = $this->getFile();
+		file_put_contents($file, $token);
+		echo 'done';
+	}
+
+	public function acmeChallenge(){
+		//var_dump("sslVerify");
+		$file = $this->getFile();
+		// get token
+		$token = @file_get_contents( $file );
+		echo $token;
+	}
+
+	public function getFile(){
+		$dir = sys_get_temp_dir(); // option...
+		$sitename = trim( strtolower( $GLOBALS['config']['main']['site_name'] ) );
+		$file = $dir ."/". $sitename .".letsencrypt";
+		return $file;
+	}
+
+}
+
+?>
