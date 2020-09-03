ebc61a179185a69ac5991fcbe3b1899154a8bfc5
Makis Tracend <makis.tracend@gmail.com>
2020-09-03 18:44:45 +1000

    :zzz: using  instead of global

--- helpers/letsencrypt.php
+++ helpers/letsencrypt.php
@@ -28,7 +28,7 @@ class LetsEncrypt {
 
 	public function getFile($maindomain=false){
 		$dir = sys_get_temp_dir(); // option...
-		$host = ( !empty($_SERVER['HTTP_HOST']) && !$maindomain) ? $_SERVER['HTTP_HOST'] : $GLOBALS['SERVER_NAME'];
+		$host = ( !empty($_SERVER['HTTP_HOST']) && !$maindomain) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
 		// FIX: sometimes the $_SERVER data is not available...
 		if( empty($host) ) $host = $GLOBALS['config']['main']['site_name'];
 		$filename = trim( strtolower( $host ) ) .".letsencrypt";
