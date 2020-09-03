329c13da8a89c2f9e0e891f860eb6de011e790c0
Makis Tracend <makis.tracend@gmail.com>
2020-09-03 18:27:30 +1000

    :zap: fallback to server name in case subdomain token is empty

--- helpers/letsencrypt.php
+++ helpers/letsencrypt.php
@@ -16,13 +16,21 @@ class LetsEncrypt {
 		$file = $this->getFile();
 		// get token
 		$token = @file_get_contents( $file );
+		// FIX: fallback to the main domain if the token is empty
+		if( empty($token) ){
+			$file = $this->getFile(true);
+			// get token
+			$token = @file_get_contents( $file );
+		}
 		echo $token;
 		exit();
 	}
 
-	public function getFile(){
-		$dir = sys_get_temp_dir(); // option...			     
-		$host = ( !empty($_SERVER['HTTP_HOST']) ) ? $_SERVER['HTTP_HOST'] : $GLOBALS['config']['main']['site_name'];			     
+	public function getFile($maindomain=false){
+		$dir = sys_get_temp_dir(); // option...
+		$host = ( !empty($_SERVER['HTTP_HOST']) && !$maindomain) ? $_SERVER['HTTP_HOST'] : $GLOBALS['SERVER_NAME'];
+		// FIX: sometimes the $_SERVER data is not available...
+		if( empty($host) ) $host = $GLOBALS['config']['main']['site_name'];
 		$filename = trim( strtolower( $host ) ) .".letsencrypt";
 		$file = $dir ."/". $filename;
 		return $file;
